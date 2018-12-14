feb9301e9054b53283220ad56389d016a00bb462
dyj360 <1362755434@qq.com>
2018-12-14 20:49:42 +0800

    这是一个学生类

---
+++ src/main/java/com/tt/zs/ZsApplication.java
@@ -0,0 +1,14 @@
+package com.tt.zs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZsApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/com/tt/zs/pojo/student.java
@@ -0,0 +1,9 @@
+package com.tt.zs.pojo;
+/**
+*@作者：duanyongjie
+*@时间：2018/12/14 20:40
+*@描述：这是一个学生类
+*/
+public class student {
+
+}
